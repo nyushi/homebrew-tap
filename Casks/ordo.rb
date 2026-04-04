44307a5601ef74cde43ce94d8b15c66d654b3495
Anka <runner@iad20-gt1024-2cfd89fb-284f-4904-8c96-8e97f303b776-F26FF81D435D.local>
2026-04-04 15:26:59 +0000

    Update ordo to 0.1.4

--- Casks/ordo.rb
+++ Casks/ordo.rb
@@ -1,8 +1,8 @@
 cask "ordo" do
-  version "0.1.3"
-  sha256 "0a044ddb419aaaa623a22c7e9b729a085efffb9bad197393281900f1cdc5517d"
+  version "0.1.4"
+  sha256 "f12c39d07c848417e18af3d708eb213a7b73fd817010b7b81e4079e97aa1b9af"
 
-  url "https://github.com/nyushi/ordo/releases/download/v0.1.3/Ordo.app.zip"
+  url "https://github.com/nyushi/ordo/releases/download/v0.1.4/Ordo.app.zip"
   name "Ordo"
   desc "Floating org-mode scratchpad for macOS"
   homepage "https://github.com/nyushi/ordo"
