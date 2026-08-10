8509280955b8cdd9fd97eafad57c7369232ee846
Yushi Nakai <nyushi@users.noreply.github.com>
2026-08-10 22:00:05 +0900

    Host cask asset in public tap release

--- Casks/27m2v-display-switch.rb
+++ Casks/27m2v-display-switch.rb
@@ -2,10 +2,10 @@ cask "27m2v-display-switch" do
   version "0.1.2"
   sha256 "6a681b9b55b4d3071b4e808441d8801024b6289536d3751f7248fe41c1520ccd"
 
-  url "https://github.com/nyushi/27m2v-display-switch/releases/download/v#{version}/27M2V-Display-Switch-v#{version}-macOS-arm64.zip"
+  url "https://github.com/nyushi/homebrew-tap/releases/download/27m2v-display-switch-v#{version}/27M2V-Display-Switch-v#{version}-macOS-arm64.zip"
   name "27M2V Display Switch"
   desc "Menu bar app for enabling and disabling an INNOCN 27M2V display"
-  homepage "https://github.com/nyushi/27m2v-display-switch"
+  homepage "https://github.com/nyushi/homebrew-tap"
 
   depends_on arch: :arm64
   depends_on macos: :ventura
